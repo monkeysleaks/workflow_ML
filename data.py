b4ecbf48c9c2a7fdc01402b5d2fe09f6dafb5b77
soteiro <diego_sarq@hotmail.com>
2025-03-06 19:55:45 -0300

    prueba 2 test ML

--- data.py
+++ data.py
@@ -6,10 +6,10 @@ import os
 import time
 from dotenv import load_dotenv
 
-#cargar variables de entorno desde un archivo .env (solo en local)
+# Cargar variables de entorno desde un archivo .env (solo en local)
 load_dotenv()
 
-#crear cliente
+# Crear cliente
 url: str = os.environ.get("DB_URL")
 key: str = os.environ.get("DB_KEY")
 supabase: Client = create_client(
@@ -35,31 +35,20 @@ def get_data_vid(tabla_vid):
     return response.data
 
 if __name__ == "__main__":
-    """
-        1.- obtener nombres
-        2.- iniciar bucle para nombres
-        3.- obtener codigos
-        4.- hacer request para los videos
-
-    """
-
     with open("resultado.txt", "w") as f:
         f.write("")
+        print("Iniciando")
 
     artistas = get_data_ar("artistas")
     for artista in artistas:
+        print("iniciando artistas")
         videos = get_data_vid(artista["nombre"])
         print(artista["nombre"])
         for video in videos:
-            res = requests.get(f"https://voe.sx/{video["code_voe"]}")
+            res = requests.get(f"https://voe.sx/{video['code_voe']}")
             if res.status_code == 200:
                 print(f"Encontrado {video['code_voe']}")
-                
             else:
                 with open("resultado.txt", "a") as f:
-                    f.write(f"artista: {artista["nombre"]}, title: {video['title']}\n") 
-                    f.close()
+                    f.write(f"artista: {artista['nombre']}, title: {video['title']}\n") 
                 print(f"No encontrado {video['title']}")
-    
-
-    
